afc9ca32ea9c9406482d70fec1b74eec0699fbf7
Arsen Cameron <arsencameron@Arsens-MacBook-Pro.local>
2025-01-18 00:27:00 -0500

    added pages

---
+++ react-app/src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Signup Page
+      </header>
+    </div>
+  );
+}
+
+export default Signup;
