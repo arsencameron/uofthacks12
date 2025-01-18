7385d4121ada0e346646082000c1c77d7b90e15c
Arsen Cameron <arsencameron@Arsens-MacBook-Pro.local>
2025-01-18 01:27:16 -0500

    added map and search bar

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import SearchBar from '../components/searchbar';
+import GoogleMap from '../components/GoogleMap';
 
 function Home() {
+  const handleSearch = (searchTerm) => {
+    console.log('Search Term:', searchTerm);
+    // Add your search logic here
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         Home Page
       </header>
+      <SearchBar onSearch={handleSearch} />
+      <GoogleMap />
     </div>
   );
 }
